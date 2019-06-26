9285df4650c31fef78b6f58d8d6c2cad48bd02a1
Gino Chirinos <gino.chirinos@ticmash.com>
2019-06-26 16:14:38 +0200

    Configuración

---
+++ src/app/vendor.js
@@ -0,0 +1,12 @@
+// Jquery
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+// Tether
+import tether from 'tether';
+window.Tether = tether;
+
+// Bootstrap
+require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const config = require('./config.json')
+const environment = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: [
+    "./src/app/vendor.js",
+    "./src/app/index.js"
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/public/index.html" }),
+    new ExtractTextPlugin("styles.css"),
+    new webpack.DefinePlugin({
+      API_URL: JSON.stringify(config.API_URL[environment])
+    }),
+    new UglifyJsPlugin({ uglifyOptions: 
+      { mangle: false }
+    }),
+  ],
+  devServer: {
+    contentBase:  path.join(__dirname, 'src/public'),
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.html$/,
+        use: "raw-loader"
+      },
+    ]
+  }
+};
